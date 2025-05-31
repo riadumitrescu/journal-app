e18b179113e7fd10eb69aebb46bec243a5d534a8
Maria Dumitrescu <mariadumitrescuoffice@gmail.com>
2025-05-31 21:11:33 +0200

    fix: add public directory and next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    domains: ['tlisradyhpaqlzxbblhm.supabase.co'],
+  },
+}
+
+module.exports = nextConfig 
